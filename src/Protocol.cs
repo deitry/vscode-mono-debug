e4d7e348a17630fd1bde9b80063c29a835ea1a97
nicklas <miniwolf1508@gmail.com>
2018-10-04 15:44:09 +0200

    Resolving issue with breakpoints being deleted for all other than newest files.
    Added error handling to breakpoints.
    Updated logging for process and player connection discovery.

--- src/Protocol.cs
+++ src/Protocol.cs
@@ -2,6 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+
 using System;
 using System.Text;
 using System.IO;
@@ -13,325 +14,363 @@ using UnityDebug;
 
 namespace VSCodeDebug
 {
-	public class ProtocolMessage
-	{
-		public int seq;
-		public string type;
+    public class ProtocolMessage
+    {
+        public int seq;
+        public string type;
 
-		public ProtocolMessage() {
-		}
-		public ProtocolMessage(string typ) {
-			type = typ;
-		}
-		public ProtocolMessage(string typ, int sq) {
-			type = typ;
-			seq = sq;
-		}
-	}
+        public ProtocolMessage() { }
 
-	public class Request : ProtocolMessage
-	{
-		public string command;
-		public dynamic arguments;
+        public ProtocolMessage(string typ)
+        {
+            type = typ;
+        }
 
-		public Request() {
-		}
-		public Request(string cmd, dynamic arg) : base("request") {
-			command = cmd;
-			arguments = arg;
-		}
-		public Request(int id, string cmd, dynamic arg) : base("request", id) {
-			command = cmd;
-			arguments = arg;
-		}
-	}
+        public ProtocolMessage(string typ, int sq)
+        {
+            type = typ;
+            seq = sq;
+        }
+    }
 
-	/** Properties of a breakpoint or logpoint passed to the setBreakpoints request. */
-	public class SourceBreakpoint
-	{
-		public int line;
-		public int column;
-		public string condition;
-		public string hitCondition;
-		public string logMessage;
-	}
+    public class Request : ProtocolMessage
+    {
+        public string command;
+        public dynamic arguments;
 
-	/*
-	 * subclasses of ResponseBody are serialized as the body of a response.
-	 * Don't change their instance variables since that will break the debug protocol.
-	 */
-	public class ResponseBody {
-		// empty
-	}
+        public Request() { }
 
-	public class Response : ProtocolMessage
-	{
-		public bool success;
-		public string message;
-		public int request_seq;
-		public string command;
-		public ResponseBody body;
+        public Request(string cmd, dynamic arg)
+            : base("request")
+        {
+            command = cmd;
+            arguments = arg;
+        }
 
-		public Response() {
-		}
-		public Response(Request req) : base("response") {
-			success = true;
-			request_seq = req.seq;
-			command = req.command;
-		}
+        public Request(int id, string cmd, dynamic arg)
+            : base("request", id)
+        {
+            command = cmd;
+            arguments = arg;
+        }
+    }
 
-		public void SetBody(ResponseBody bdy) {
-			success = true;
-			body = bdy;
-		}
+    /** Properties of a breakpoint or logpoint passed to the setBreakpoints request. */
+    public class SourceBreakpoint
+    {
+        public int line;
+        public int column;
+        public string condition;
+        public string hitCondition;
+        public string logMessage;
+    }
 
-		public void SetErrorBody(string msg, ResponseBody bdy = null) {
-			success = false;
-			message = msg;
-			body = bdy;
-		}
-	}
+    /*
+     * subclasses of ResponseBody are serialized as the body of a response.
+     * Don't change their instance variables since that will break the debug protocol.
+     */
+    public class ResponseBody
+    {
+        // empty
+    }
 
-	public class Event : ProtocolMessage
-	{
-		[JsonProperty(PropertyName = "event")]
-		public string eventType { get; }
-		public dynamic body { get; }
+    public class Response : ProtocolMessage
+    {
+        public bool success;
+        public string message;
+        public int request_seq;
+        public string command;
+        public ResponseBody body;
 
-		public Event(string type, dynamic bdy = null) : base("event") {
-			eventType = type;
-			body = bdy;
-		}
-	}
+        public Response() { }
 
-	/*
-	 * The ProtocolServer can be used to implement a server that uses the VSCode debug protocol.
-	 */
-	public abstract class ProtocolServer
-	{
-		public bool TRACE;
-		public bool TRACE_RESPONSE;
+        public Response(Request req)
+            : base("response")
+        {
+            success = true;
+            request_seq = req.seq;
+            command = req.command;
+        }
 
-		protected const int BUFFER_SIZE = 4096;
-		protected const string TWO_CRLF = "\r\n\r\n";
-		protected static Regex CONTENT_LENGTH_MATCHER;
+        public void SetBody(ResponseBody bdy)
+        {
+            success = true;
+            body = bdy;
+        }
 
-		protected static Encoding Encoding = System.Text.Encoding.UTF8;
+        public void SetErrorBody(string msg, ResponseBody bdy = null)
+        {
+            success = false;
+            message = msg;
+            body = bdy;
+        }
+    }
 
-		private int _sequenceNumber;
-		private Dictionary<int, TaskCompletionSource<Response>> _pendingRequests;
+    public class Event : ProtocolMessage
+    {
+        [JsonProperty(PropertyName = "event")]
+        public string eventType { get; }
 
-		private Stream _outputStream;
+        public dynamic body { get; }
 
-		private ByteBuffer _rawData;
-		private int _bodyLength;
+        public Event(string type, dynamic bdy = null)
+            : base("event")
+        {
+            eventType = type;
+            body = bdy;
+        }
+    }
 
-		private bool _stopRequested;
+    /*
+     * The ProtocolServer can be used to implement a server that uses the VSCode debug protocol.
+     */
+    public abstract class ProtocolServer
+    {
+        public bool TRACE;
+        public bool TRACE_RESPONSE;
 
+        protected const int BUFFER_SIZE = 4096;
+        protected const string TWO_CRLF = "\r\n\r\n";
+        protected static Regex CONTENT_LENGTH_MATCHER;
 
-		public ProtocolServer()
-		{
-			CONTENT_LENGTH_MATCHER = new Regex(@"Content-Length: (\d+)");
-			Encoding = Encoding.UTF8;
-			_sequenceNumber = 1;
-			_bodyLength = -1;
-			_rawData = new ByteBuffer();
-			_pendingRequests = new Dictionary<int, TaskCompletionSource<Response>>();
-		}
+        protected static Encoding Encoding = System.Text.Encoding.UTF8;
 
-		public async Task Start(Stream inputStream, Stream outputStream)
-		{
-			_outputStream = outputStream;
+        private int _sequenceNumber;
+        private Dictionary<int, TaskCompletionSource<Response>> _pendingRequests;
 
-			byte[] buffer = new byte[BUFFER_SIZE];
+        private Stream _outputStream;
 
-			_stopRequested = false;
-			while (!_stopRequested) {
-				var read = await inputStream.ReadAsync(buffer, 0, buffer.Length);
+        private ByteBuffer _rawData;
+        private int _bodyLength;
 
-				if (read == 0) {
-					// end of stream
-					break;
-				}
+        private bool _stopRequested;
 
-				if (read > 0) {
-					_rawData.Append(buffer, read);
-					ProcessData();
-				}
-			}
-		}
+        public ProtocolServer()
+        {
+            CONTENT_LENGTH_MATCHER = new Regex(@"Content-Length: (\d+)");
+            Encoding = Encoding.UTF8;
+            _sequenceNumber = 1;
+            _bodyLength = -1;
+            _rawData = new ByteBuffer();
+            _pendingRequests = new Dictionary<int, TaskCompletionSource<Response>>();
+        }
 
-		public void Stop()
-		{
-			_stopRequested = true;
-		}
+        public async Task Start(Stream inputStream, Stream outputStream)
+        {
+            _outputStream = outputStream;
 
-		public void SendEvent(Event e)
-		{
-			SendMessage(e);
-		}
+            byte[] buffer = new byte[BUFFER_SIZE];
 
-		public Task<Response> SendRequest(string command, dynamic args)
-		{
-			var tcs = new TaskCompletionSource<Response>();
+            _stopRequested = false;
+            while (!_stopRequested)
+            {
+                var read = await inputStream.ReadAsync(buffer, 0, buffer.Length);
 
-			Request request = null;
-			lock (_pendingRequests) {
-				request = new Request(_sequenceNumber++, command, args);
-				// wait for response
-				_pendingRequests.Add(request.seq, tcs);
-			}
+                if (read == 0)
+                {
+                    // end of stream
+                    break;
+                }
 
-			SendMessage(request);
+                if (read > 0)
+                {
+                    _rawData.Append(buffer, read);
+                    ProcessData();
+                }
+            }
+        }
 
-			return tcs.Task;
-		}
+        public void Stop()
+        {
+            _stopRequested = true;
+        }
 
-		protected abstract void DispatchRequest(string command, dynamic args, Response response);
+        public void SendEvent(Event e)
+        {
+            SendMessage(e);
+        }
 
-		// ---- private ------------------------------------------------------------------------
+        public Task<Response> SendRequest(string command, dynamic args)
+        {
+            var tcs = new TaskCompletionSource<Response>();
 
-		private void ProcessData()
-		{
-			while (true) {
-				if (_bodyLength >= 0) {
-					if (_rawData.Length >= _bodyLength) {
-						var buf = _rawData.RemoveFirst(_bodyLength);
+            Request request = null;
+            lock (_pendingRequests)
+            {
+                request = new Request(_sequenceNumber++, command, args);
 
-						_bodyLength = -1;
+                // wait for response
+                _pendingRequests.Add(request.seq, tcs);
+            }
 
-						Dispatch(Encoding.GetString(buf));
+            SendMessage(request);
 
-						continue;   // there may be more complete messages to process
-					}
-				}
-				else {
-					string s = _rawData.GetString(Encoding);
-					var idx = s.IndexOf(TWO_CRLF);
-					if (idx != -1) {
-						Match m = CONTENT_LENGTH_MATCHER.Match(s);
-						if (m.Success && m.Groups.Count == 2) {
-							_bodyLength = Convert.ToInt32(m.Groups[1].ToString());
+            return tcs.Task;
+        }
 
-							_rawData.RemoveFirst(idx + TWO_CRLF.Length);
+        protected abstract void DispatchRequest(string command, dynamic args, Response response);
 
-							continue;   // try to handle a complete message
-						}
-					}
-				}
-				break;
-			}
-		}
+        // ---- private ------------------------------------------------------------------------
 
-		private void Dispatch(string req)
-		{
-			var message = JsonConvert.DeserializeObject<ProtocolMessage>(req);
-			if (message != null) {
-				switch (message.type) {
+        private void ProcessData()
+        {
+            while (true)
+            {
+                if (_bodyLength >= 0)
+                {
+                    if (_rawData.Length >= _bodyLength)
+                    {
+                        var buf = _rawData.RemoveFirst(_bodyLength);
 
-				case "request":
-					{
-						var request = JsonConvert.DeserializeObject<Request>(req);
+                        _bodyLength = -1;
 
-						Log.Write($"Command: {request.command}: {JsonConvert.SerializeObject(request.arguments, Formatting.Indented)}");
+                        Dispatch(Encoding.GetString(buf));
 
-						var response = new Response(request);
-						DispatchRequest(request.command, request.arguments, response);
-						SendMessage(response);
-					}
-					break;
+                        continue; // there may be more complete messages to process
+                    }
+                }
+                else
+                {
+                    string s = _rawData.GetString(Encoding);
+                    var idx = s.IndexOf(TWO_CRLF);
+                    if (idx != -1)
+                    {
+                        Match m = CONTENT_LENGTH_MATCHER.Match(s);
+                        if (m.Success && m.Groups.Count == 2)
+                        {
+                            _bodyLength = Convert.ToInt32(m.Groups[1].ToString());
 
-				case "response":
-					{
-						var response = JsonConvert.DeserializeObject<Response>(req);
-						int seq = response.request_seq;
-						lock (_pendingRequests) {
-							if (_pendingRequests.ContainsKey(seq)) {
-								var tcs = _pendingRequests[seq];
-								_pendingRequests.Remove(seq);
-								tcs.SetResult(response);
-							}
-						}
-					}
-					break;
-				}
-			}
-		}
+                            _rawData.RemoveFirst(idx + TWO_CRLF.Length);
 
-		protected void SendMessage(ProtocolMessage message)
-		{
-			if (message.seq == 0) {
-				message.seq = _sequenceNumber++;
-			}
+                            continue; // try to handle a complete message
+                        }
+                    }
+                }
 
-			Program.Log(TRACE_RESPONSE && message.type == "response", " R: {0}", JsonConvert.SerializeObject(message, Formatting.Indented));
+                break;
+            }
+        }
 
-			if (message.type == "event" && message is Event) {
-				var e = message as Event;
-				Program.Log(TRACE, "E {0}: {1}", ((Event)message).eventType, JsonConvert.SerializeObject(e.body, Formatting.Indented));
-			}
+        private void Dispatch(string req)
+        {
+            var message = JsonConvert.DeserializeObject<ProtocolMessage>(req);
+            if (message != null)
+            {
+                switch (message.type)
+                {
+                    case "request":
+                    {
+                        var request = JsonConvert.DeserializeObject<Request>(req);
 
-			var data = ConvertToBytes(message);
-			try {
-				_outputStream.Write(data, 0, data.Length);
-				_outputStream.Flush();
-			}
-			catch (Exception) {
-				// ignore
-			}
-		}
+                        Log.Write($"Command: {request.command}: {JsonConvert.SerializeObject(request.arguments, Formatting.Indented)}");
 
-		private static byte[] ConvertToBytes(ProtocolMessage request)
-		{
-			var asJson = JsonConvert.SerializeObject(request);
-			byte[] jsonBytes = Encoding.GetBytes(asJson);
+                        var response = new Response(request);
+                        DispatchRequest(request.command, request.arguments, response);
+                        SendMessage(response);
+                    }
+                        break;
 
-			string header = string.Format("Content-Length: {0}{1}", jsonBytes.Length, TWO_CRLF);
-			byte[] headerBytes = Encoding.GetBytes(header);
+                    case "response":
+                    {
+                        var response = JsonConvert.DeserializeObject<Response>(req);
 
-			byte[] data = new byte[headerBytes.Length + jsonBytes.Length];
-			System.Buffer.BlockCopy(headerBytes, 0, data, 0, headerBytes.Length);
-			System.Buffer.BlockCopy(jsonBytes, 0, data, headerBytes.Length, jsonBytes.Length);
+                        Log.Write($"Command: {response.command}: {JsonConvert.SerializeObject(response.message, Formatting.Indented)}");
+                        int seq = response.request_seq;
+                        lock (_pendingRequests)
+                        {
+                            if (_pendingRequests.ContainsKey(seq))
+                            {
+                                var tcs = _pendingRequests[seq];
+                                _pendingRequests.Remove(seq);
+                                tcs.SetResult(response);
+                            }
+                        }
+                    }
+                        break;
+                }
+            }
+        }
 
-			return data;
-		}
-	}
+        protected void SendMessage(ProtocolMessage message)
+        {
+            if (message.seq == 0)
+            {
+                message.seq = _sequenceNumber++;
+            }
 
-	//--------------------------------------------------------------------------------------
+            Program.Log(TRACE_RESPONSE && message.type == "response", " R: {0}", JsonConvert.SerializeObject(message, Formatting.Indented));
 
-	class ByteBuffer
-	{
-		private byte[] _buffer;
+            if (message.type == "event" && message is Event)
+            {
+                var e = message as Event;
+                Program.Log(TRACE, "E {0}: {1}", ((Event)message).eventType, JsonConvert.SerializeObject(e.body, Formatting.Indented));
+            }
 
-		public ByteBuffer() {
-			_buffer = new byte[0];
-		}
+            var data = ConvertToBytes(message);
+            try
+            {
+                _outputStream.Write(data, 0, data.Length);
+                _outputStream.Flush();
+            }
+            catch (Exception)
+            {
+                // ignore
+            }
+        }
 
-		public int Length {
-			get { return _buffer.Length; }
-		}
+        private static byte[] ConvertToBytes(ProtocolMessage request)
+        {
+            var asJson = JsonConvert.SerializeObject(request);
+            byte[] jsonBytes = Encoding.GetBytes(asJson);
 
-		public string GetString(Encoding enc)
-		{
-			return enc.GetString(_buffer);
-		}
+            string header = string.Format("Content-Length: {0}{1}", jsonBytes.Length, TWO_CRLF);
+            byte[] headerBytes = Encoding.GetBytes(header);
 
-		public void Append(byte[] b, int length)
-		{
-			byte[] newBuffer = new byte[_buffer.Length + length];
-			System.Buffer.BlockCopy(_buffer, 0, newBuffer, 0, _buffer.Length);
-			System.Buffer.BlockCopy(b, 0, newBuffer, _buffer.Length, length);
-			_buffer = newBuffer;
-		}
+            byte[] data = new byte[headerBytes.Length + jsonBytes.Length];
+            System.Buffer.BlockCopy(headerBytes, 0, data, 0, headerBytes.Length);
+            System.Buffer.BlockCopy(jsonBytes, 0, data, headerBytes.Length, jsonBytes.Length);
 
-		public byte[] RemoveFirst(int n)
-		{
-			byte[] b = new byte[n];
-			System.Buffer.BlockCopy(_buffer, 0, b, 0, n);
-			byte[] newBuffer = new byte[_buffer.Length - n];
-			System.Buffer.BlockCopy(_buffer, n, newBuffer, 0, _buffer.Length - n);
-			_buffer = newBuffer;
-			return b;
-		}
-	}
+            return data;
+        }
+    }
+
+    //--------------------------------------------------------------------------------------
+
+    class ByteBuffer
+    {
+        private byte[] _buffer;
+
+        public ByteBuffer()
+        {
+            _buffer = new byte[0];
+        }
+
+        public int Length
+        {
+            get { return _buffer.Length; }
+        }
+
+        public string GetString(Encoding enc)
+        {
+            return enc.GetString(_buffer);
+        }
+
+        public void Append(byte[] b, int length)
+        {
+            byte[] newBuffer = new byte[_buffer.Length + length];
+            System.Buffer.BlockCopy(_buffer, 0, newBuffer, 0, _buffer.Length);
+            System.Buffer.BlockCopy(b, 0, newBuffer, _buffer.Length, length);
+            _buffer = newBuffer;
+        }
+
+        public byte[] RemoveFirst(int n)
+        {
+            byte[] b = new byte[n];
+            System.Buffer.BlockCopy(_buffer, 0, b, 0, n);
+            byte[] newBuffer = new byte[_buffer.Length - n];
+            System.Buffer.BlockCopy(_buffer, n, newBuffer, 0, _buffer.Length - n);
+            _buffer = newBuffer;
+            return b;
+        }
+    }
 }
